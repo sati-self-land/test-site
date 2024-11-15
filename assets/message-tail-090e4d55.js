540d6364e7a76b9d5ab7c67df7b0fdfd65b651a2
sati-self-land <satiselfland@gmail.com>
2024-11-15 03:14:28 +0000

    Auto-deployed from Repository Wuhe

--- assets/message-tail-090e4d55.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as o,e as s}from"./index-5ff55b05.js";const a={xmlns:"http://www.w3.org/2000/svg","data-name":"圖層 2",viewBox:"0 0 15.06 17.47"};function r(n,e){return t(),o("svg",a,e[0]||(e[0]=[s("path",{d:"M13.73 1.4C9.96 2.75 4.84 3.39 0 0c0 0 4.59 14.89 15.06 17.47",style:{fill:"#fff","stroke-width":"0"}},null,-1)]))}const c={render:r};export{c as default,r as render};
